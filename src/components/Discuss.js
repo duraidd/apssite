c6508eacba925c3dfbd082be28d5d7e685a86014
durai <duraiessakimuthu@gmail.com>
2024-08-17 02:26:05 +0530

    update

--- src/components/Discuss.js
+++ src/components/Discuss.js
@@ -2,7 +2,7 @@ import { Button } from '@mui/material'
 import React from 'react'
 import { Box } from '@mui/material'
 import Img1 from '../../src/asset/letstry.png'
-import Img2 from '../asset/dev.jpg'
+import Img2 from '../asset/dev.jpeg'
 
 
 function Discuss(props) {
@@ -12,7 +12,7 @@ function Discuss(props) {
     }
 
     return (
-        <div style={{ display: 'flex', flexDirection: "row", padding: "5% 8%",width:"100vw" }} >
+        <div style={{ display: 'flex', flexDirection: "row", padding: "5% 9%",width:"100vw" }} >
             <Box sx={{
                 '@media (max-width: 425px)': {
                     display: 'none',
@@ -28,7 +28,7 @@ function Discuss(props) {
                         display: 'none',
                     }
                 }}>
-                    <img src={Img2} alt="Profile" width="200" height="250" style={{ objectFit: "cover", borderRadius: "50%", border: "2px dashed red", padding: "10px" }} />
+                    <img src={Img2} alt="Profile" width="240" height="250" style={{ objectFit: "cover", borderRadius: "50%", border: "2px dashed red", padding: "10px" }} />
                 </Box>
                 <Box sx={{ border: "1px solid #FF2A53", padding: "2rem", borderRadius: "10px", "&:hover": { boxShadow: "8px 12px 12px #f8a6b6" } }} >
                     <h3 style={{ color: "#FF2A53" }}>At MuleSoft IT Services, we thrive on challenges and <br /> are passionate about delivering exceptional results.</h3>
